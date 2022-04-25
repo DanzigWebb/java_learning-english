d91e8b89d22727be3b516817b2a1fdc0250f2f49
Alexander <valverin95@gmail.com>
2022-04-25 05:19:32 +0300

    feat: [FE] добавлены модельки

---
+++ ui/src/app/models/page/page.type.ts
@@ -0,0 +1,22 @@
+export interface PageParams {
+  page?: number;
+  size?: number;
+  sortBy?: string;
+}
+
+export interface Page<T> {
+  content: T[];
+  empty: boolean;
+  first: boolean;
+  last: boolean;
+  number: number;
+  numberOfElements: number;
+  size: number;
+  sort: {
+    empty: boolean,
+    sorted: boolean,
+    unsorted: boolean
+  };
+  totalElements: number;
+  totalPages: number;
+}
---
+++ ui/src/app/models/word/word.type.ts
@@ -0,0 +1,42 @@
+/**
+ * @See from Java model: rest.word.model.Word
+ */
+export interface WordDto {
+  id: string;
+  groupId: string;
+  name: string;
+  definition: string;
+  done: boolean;
+  createAt: string;
+  updateAt: string;
+
+  rank: string;
+  associate: string;
+}
+
+export interface WordCreateDto {
+  groupId?: string;
+  name: string;
+  definition?: string;
+  associate?: string;
+  done?: boolean;
+  rank?: string;
+}
+
+/**
+ * @See from Java model: rest.group.model.GroupCard
+ */
+export interface WordGroupDto {
+  id: string;
+  name: string;
+  createAt: string;
+  updateAt: string;
+  done: boolean;
+  archived: boolean;
+  words: WordDto[];
+}
+
+export interface WordGroupCreateDto {
+  name: string;
+  done?: boolean;
+}
